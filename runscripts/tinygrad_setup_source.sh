24fab34d1a23c5e2d42d5b2cff28c43b08f03052
Swarnim Arun <swarnimarun11@gmail.com>
2023-10-20 12:31:35 +0530

    remove old runscripts

--- runscripts/tinygrad_setup_source.sh
+++
@@ -1,7 +0,0 @@
-## source this!
-## DO NOT EXECUTE!
-
-git clone --depth=1 https://github.com/tinygrad/tinygrad.git /tmp/tinygrad
-python -m venv /tmp/tinygrad/.venv
-source /tmp/tinygrad/.venv/bin/activate
-python3 -m pip install -e /tmp/tinygrad
