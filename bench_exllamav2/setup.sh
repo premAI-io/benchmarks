04232831f090dade4f973d7d4b37562b8fea6e0f
Anindyadeep <proanindyadeep@gmail.com>
2023-12-06 13:32:39 +0000

    initial setup script for exllamav2

---
+++ bench_exllamav2/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+################################################################################
+# Script: setup.sh <DEVICE>
+# Description: Automates the setup of a virtual environment and installs project
+# requirements.
+################################################################################
