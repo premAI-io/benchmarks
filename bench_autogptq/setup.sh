139a51b3c6b7508decb268d0e1488b6ff23eb534
Anindyadeep <proanindyadeep@gmail.com>
2023-12-21 09:48:17 +0000

    setup can now support for python and python3

--- bench_autogptq/setup.sh
+++ bench_autogptq/setup.sh
@@ -13,23 +13,35 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 VENV_DIR="$SCRIPT_DIR/venv"
 GPTQ_WEIGHTS_FOLDER="${GPTQ_WEIGHTS_FOLDER:-"./models/llama-2-7b-gptq"}"
 
+check_python() {
+    if command -v python &> /dev/null; then
+        PYTHON_CMD="python"
+    elif command -v python3 &> /dev/null; then
+        PYTHON_CMD="python3"
+    else
+        echo "Python is not installed."
+        exit 1
+    fi
+}
+
 download_gptq_weights() {
     # download the sample file if not exists
     if [ ! -d "$GPTQ_WEIGHTS_FOLDER" ]; then
-    huggingface-cli download TheBloke/Llama-2-7B-GPTQ --local-dir ./models/llama-2-7b-autogptq --exclude "*.git*" "*.md" "Notice" "LICENSE"
+        huggingface-cli download TheBloke/Llama-2-7B-GPTQ --local-dir ./models/llama-2-7b-autogptq --exclude "*.git*" "*.md" "Notice" "LICENSE"
     else
         echo "Weights already downloaded!"
     fi
 }
 
+check_python
 
 if [ ! -d "$VENV_DIR" ]; then
-    python -m venv "$VENV_DIR"
+    "$PYTHON_CMD" -m venv "$VENV_DIR"
     echo "Virtual environment '$VENV_DIR' created."
     # shellcheck disable=SC1091
     source "$VENV_DIR/bin/activate"
-    pip install --upgrade pip > /dev/null
-    pip install -r "$SCRIPT_DIR/requirements.txt" --no-cache-dir > /dev/null
+    "$PYTHON_CMD" -m pip install --upgrade pip > /dev/null
+    "$PYTHON_CMD" -m pip install -r "$SCRIPT_DIR/requirements.txt" --no-cache-dir > /dev/null
 else
     # shellcheck disable=SC1091
     source "$VENV_DIR/bin/activate"
