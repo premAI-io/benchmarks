4dfc0e46e3f8803ff51d46efd05f2070eeb33811
Anindyadeep <proanindyadeep@gmail.com>
2024-01-09 18:27:00 +0000

    added docke based installation scripts

--- bench_optimum_nvidia/setup.sh
+++ bench_optimum_nvidia/setup.sh
@@ -9,17 +9,42 @@
 set -euo pipefail
 
 # Main script starts here.
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-VENV_DIR="$SCRIPT_DIR/venv"
 
-if [ ! -d "$VENV_DIR" ]; then
-    python -m venv "$VENV_DIR"
-    echo "Virtual environment '$VENV_DIR' created."
-    # shellcheck disable=SC1091
-    source "$VENV_DIR/bin/activate"
-    pip install --upgrade pip > /dev/null
-    pip install -r "$SCRIPT_DIR/requirements.txt" --no-cache-dir > /dev/null
-else
-    # shellcheck disable=SC1091
-    source "$VENV_DIR/bin/activate"
-fi
+CURRENT_DIR="$(pwd)"
+
+check_docker () {
+    pass
+}
+
+build_docker_image () {
+    # Check if the Docker image exists
+    if docker image inspect prem/optimum-nvidia:latest &> /dev/null; then
+        echo "Image 'prem/optimum-nvidia:latest' already exists."
+        exit 1
+    else
+        # Clone the repository and build the Docker image
+        git clone --recursive --depth=1 https://github.com/huggingface/optimum-nvidia.git
+        cd optimum-nvidia/third-party/tensorrt-llm
+        make -C docker release_build
+        cd ../.. && docker build -t prem/optimum-nvidia:base -f docker/Dockerfile .
+    fi
+
+    cd "$CURRENT_DIR"
+}
+
+build_and_compile_model () {
+    echo "Running ... "
+    if docker image inspect prem/optimum-nvidia:latest &> /dev/null; then
+        docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -v /home/paperspace/workspace/benchmarks/models:/models prem/optimum-nvidia:latest python3 ./text-generation/llama.py /models/llama-2-7b-hf /build
+        docker tag prem/optimum-nvidia:latest prem/optimum-nvidia:v1  # Tagging the new image as 'latest'
+        docker image rm prem/optimum-nvidia:latest -f  # Remove the old 'base' image
+    else
+        echo "The base image does not exist locally. Exiting..."
+        exit 1
+    fi
+}
+
+
+
+# build_docker_image
+build_and_compile_model
