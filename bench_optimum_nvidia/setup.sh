e42e375e82ff59e5b6bdfd0ae81b10c8a1275534
Anindyadeep <proanindyadeep@gmail.com>
2024-01-10 11:06:26 +0000

    Add: check_docker function and small bug fix

--- bench_optimum_nvidia/setup.sh
+++ bench_optimum_nvidia/setup.sh
@@ -12,9 +12,14 @@ set -euo pipefail
 
 CURRENT_DIR="$(pwd)"
 
-check_docker () {
-    pass
+check_docker() {
+    if command -v docker &> /dev/null; then
+        return 0
+    else
+        return 1
+    fi
 }
+
 
 build_docker_image () {
     # Check if the Docker image exists
@@ -34,10 +39,20 @@ build_docker_image () {
 
 build_and_compile_model () {
     echo "Running ... "
-    if docker image inspect prem/optimum-nvidia:latest &> /dev/null; then
-        docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -v /home/paperspace/workspace/benchmarks/models:/models prem/optimum-nvidia:latest python3 ./text-generation/llama.py /models/llama-2-7b-hf /build
-        docker tag prem/optimum-nvidia:latest prem/optimum-nvidia:v1  # Tagging the new image as 'latest'
-        docker image rm prem/optimum-nvidia:latest -f  # Remove the old 'base' image
+    if docker image inspect prem/optimum-nvidia:base &> /dev/null; then
+        docker run \
+        --gpus all \
+        --ipc=host \
+        --ulimit memlock=-1 \
+        --ulimit stack=67108864 \
+        -v /home/paperspace/workspace/benchmarks/models:/models \
+        prem/optimum-nvidia:base \
+        python3 ./text-generation/llama.py /models/llama-2-7b-hf /build
+        docker tag prem/optimum-nvidia:base prem/optimum-nvidia:latest
+        docker image rm prem/optimum-nvidia:base -f
+    elif docker image inspect prem/optimum-nvidia:latest &> /dev/null; then
+        echo "Image already present."
+        exit
     else
         echo "The base image does not exist locally. Exiting..."
         exit 1
@@ -45,6 +60,9 @@ build_and_compile_model () {
 }
 
 
-
-# build_docker_image
-build_and_compile_model
+if check_docker; then
+    build_docker_image
+    build_and_compile_model
+else
+    echo "Docker is not installed or not in the PATH"
+fi
