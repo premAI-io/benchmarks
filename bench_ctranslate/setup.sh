df19017215217930270bca059aa5afe0c64c175d
Anindyadeep <proanindyadeep@gmail.com>
2024-01-25 15:52:56 +0000

    fix: typo around folder name and messages

--- bench_ctranslate/setup.sh
+++ bench_ctranslate/setup.sh
@@ -31,8 +31,8 @@ build_and_compile_model () {
     set -e  # Exit on error
     echo "Running and building the model inside Docker..."
 
-    local model_build_path_32="$CURRENT_DIR/models/llama-2-7b-ctranslate2-fp32"
-    local model_build_path_16="$CURRENT_DIR/models/llama-2-7b-ctranslate2-fp16"
+    local model_build_path_32="$CURRENT_DIR/models/llama-2-7b-ctranslate2-float32"
+    local model_build_path_16="$CURRENT_DIR/models/llama-2-7b-ctranslate2-float16"
     local model_build_path_08="$CURRENT_DIR/models/llama-2-7b-ctranslate2-int8"
 
     if docker image inspect prem-ctranslate2:latest &> /dev/null; then
@@ -41,7 +41,7 @@ build_and_compile_model () {
                 --gpus=all \
                 -v "$CURRENT_DIR"/models:/models \
                 prem-ctranslate2:latest \
-                ct2-transformers-converter --model /models/llama-2-7b-hf --quantization float32 --output_dir /models/llama-2-7b-ctranslate2-fp32 --copy_files tokenizer.model --force
+                ct2-transformers-converter --model /models/llama-2-7b-hf --quantization float32 --output_dir /models/llama-2-7b-ctranslate2-float32 --copy_files tokenizer.model --force
             echo "Model build for FP32 ran successfully ... "
         fi
 
@@ -50,8 +50,8 @@ build_and_compile_model () {
                 --gpus=all \
                 -v "$CURRENT_DIR"/models:/models \
                 prem-ctranslate2:latest \
-                ct2-transformers-converter --model /models/llama-2-7b-hf --quantization float16 --output_dir /models/llama-2-7b-ctranslate2-fp16 --copy_files tokenizer.model --force
-            echo "Model build for FP32 ran successfully ... "
+                ct2-transformers-converter --model /models/llama-2-7b-hf --quantization float16 --output_dir /models/llama-2-7b-ctranslate2-float16 --copy_files tokenizer.model --force
+            echo "Model build for FP16 ran successfully ... "
         fi
 
         if [ ! -d "$model_build_path_08" ]; then
@@ -59,8 +59,8 @@ build_and_compile_model () {
                 --gpus=all \
                 -v "$CURRENT_DIR"/models:/models \
                 prem-ctranslate2:latest \
-                ct2-transformers-converter --model /models/llama-2-7b-hf --quantization int8_float16 --output_dir /models/llama-2-7b-ctranslate2-int8 --copy_files tokenizer.model --force
-            echo "Model build for FP32 ran successfully ... "
+                ct2-transformers-converter --model /models/llama-2-7b-hf --quantization int8 --output_dir /models/llama-2-7b-ctranslate2-int8 --copy_files tokenizer.model --force
+            echo "Model build for INT8 ran successfully ... "
         fi
     else
         echo "Image does not exist locally. Exiting ... "
