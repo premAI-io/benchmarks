da01884e1a9f34600252bd03725988a19d762325
Anindyadeep <proanindyadeep@gmail.com>
2024-01-31 07:29:25 +0000

    Final bug fix and cleanups in setup and model build scripts

--- bench_tensorrtllm/setup.sh
+++ bench_tensorrtllm/setup.sh
@@ -56,7 +56,7 @@ build_docker_image () {
 # build and compile different models
 
 build_engine_float32 () {
-    local model_build_path_32="$CURRENT_DIR/models/llama-2-7b-nvidia_tensorrt_build_32"
+    local model_build_path_32="$CURRENT_DIR/models/llama-2-7b-nvidia_tensorrt_build_fp32"
 
     if [ ! -d "$model_build_path_32" ]; then
         mkdir -p "$model_build_path_32"
@@ -68,12 +68,12 @@ build_engine_float32 () {
             -v "$SCRIPT_DIR"/TensorRT-LLM:/code/tensorrt_llm \
             --env "CCACHE_DIR=/code/tensorrt_llm/cpp/.ccache" \
             --env "CCACHE_BASEDIR=/code/tensorrt_llm" \
-            --workdir /code/tensorrt_llm \
+            --workdir /app/tensorrt_llm \
             --hostname psqh4m1l0zhx-release \
             --name tensorrt_llm-release-prem \
             --tmpfs /tmp:exec \
             tensorrt_llm/release:latest \
-            python3 examples/llama/build.py \
+            python3 ./examples/llama/build.py \
                 --model_dir /mnt/models/llama-2-7b-hf \
                 --dtype float32 \
                 --max_batch_size 1 \
@@ -86,7 +86,7 @@ build_engine_float32 () {
 }
 
 build_engine_float16 () {
-    local model_build_path_16="$CURRENT_DIR/models/llama-2-7b-nvidia_tensorrt_build_16"
+    local model_build_path_16="$CURRENT_DIR/models/llama-2-7b-nvidia_tensorrt_build_fp16"
 
     if [ ! -d "$model_build_path_16" ]; then
         mkdir -p "$model_build_path_16"
@@ -98,12 +98,12 @@ build_engine_float16 () {
             -v "$SCRIPT_DIR"/TensorRT-LLM:/code/tensorrt_llm \
             --env "CCACHE_DIR=/code/tensorrt_llm/cpp/.ccache" \
             --env "CCACHE_BASEDIR=/code/tensorrt_llm" \
-            --workdir /code/tensorrt_llm \
+            --workdir /app/tensorrt_llm \
             --hostname psqh4m1l0zhx-release \
             --name tensorrt_llm-release-prem \
             --tmpfs /tmp:exec \
             tensorrt_llm/release:latest \
-            python3 examples/llama/build.py \
+            python3 ./examples/llama/build.py \
                 --model_dir /mnt/models/llama-2-7b-hf \
                 --dtype float16 \
                 --max_batch_size 1 \
@@ -117,7 +117,7 @@ build_engine_float16 () {
 
 build_engine_int8 () {
 
-    local model_build_path_08="$CURRENT_DIR/models/llama-2-7b-nvidia_tensorrt_build_08"
+    local model_build_path_08="$CURRENT_DIR/models/llama-2-7b-nvidia_tensorrt_build_int8"
 
     if [ ! -d "$model_build_path_08" ]; then
         mkdir -p "$model_build_path_08"
@@ -130,11 +130,12 @@ build_engine_int8 () {
             -v "$SCRIPT_DIR"/TensorRT-LLM:/code/tensorrt_llm \
             --env "CCACHE_DIR=/code/tensorrt_llm/cpp/.ccache" \
             --env "CCACHE_BASEDIR=/code/tensorrt_llm" \
-            --workdir /code/tensorrt_llm \
+            --workdir /app/tensorrt_llm \
             --hostname psqh4m1l0zhx-release \
             --name tensorrt_llm-release-prem \
             --tmpfs /tmp:exec \
-            python3 examples/llama/hf_llama_convert.py -i /mnt/models/llama-2-7b-hf \
+            tensorrt_llm/release:latest \
+            python3 ./examples/llama/hf_llama_convert.py -i /mnt/models/llama-2-7b-hf \
                 -o /tensorrt_nvidia_build_08 \
                 --calibrate-kv-cache -t fp16
     fi
@@ -150,12 +151,12 @@ build_engine_int8 () {
             -v "$SCRIPT_DIR"/TensorRT-LLM:/code/tensorrt_llm \
             --env "CCACHE_DIR=/code/tensorrt_llm/cpp/.ccache" \
             --env "CCACHE_BASEDIR=/code/tensorrt_llm" \
-            --workdir /code/tensorrt_llm \
+            --workdir /app/tensorrt_llm \
             --hostname psqh4m1l0zhx-release \
             --name tensorrt_llm-release-prem \
             --tmpfs /tmp:exec \
             tensorrt_llm/release:latest \
-            python3 examples/llama/build.py \
+            python3 ./examples/llama/build.py \
                 --bin_model_dir /tensorrt_nvidia_build_08/1-gpu \
                 --dtype float16 \
                 --use_gpt_attention_plugin float16 \
@@ -173,7 +174,7 @@ build_engine_int8 () {
 }
 
 build_engine_int4 () {
-    local model_build_path_04="$CURRENT_DIR/models/llama-2-7b-nvidia_tensorrt_build_04"
+    local model_build_path_04="$CURRENT_DIR/models/llama-2-7b-nvidia_tensorrt_build_int4"
 
     if [ ! -d "$model_build_path_04" ]; then
         mkdir -p "$model_build_path_04"
@@ -186,12 +187,12 @@ build_engine_int4 () {
             -v "$SCRIPT_DIR"/TensorRT-LLM:/code/tensorrt_llm \
             --env "CCACHE_DIR=/code/tensorrt_llm/cpp/.ccache" \
             --env "CCACHE_BASEDIR=/code/tensorrt_llm" \
-            --workdir /code/tensorrt_llm \
+            --workdir /app/tensorrt_llm \
             --hostname psqh4m1l0zhx-release \
             --name tensorrt_llm-release-prem \
             --tmpfs /tmp:exec \
             tensorrt_llm/release:latest \
-            python3 examples/quantization/quantize.py --model_dir /mnt/models/llama-2-7b-hf \
+            python3 ./examples/quantization/quantize.py --model_dir /mnt/models/llama-2-7b-hf \
                 --dtype float16 \
                 --qformat int4_awq \
                 --export_path /tensorrt_nvidia_build_04 \
@@ -209,12 +210,12 @@ build_engine_int4 () {
             -v "$SCRIPT_DIR"/TensorRT-LLM:/code/tensorrt_llm \
             --env "CCACHE_DIR=/code/tensorrt_llm/cpp/.ccache" \
             --env "CCACHE_BASEDIR=/code/tensorrt_llm" \
-            --workdir /code/tensorrt_llm \
+            --workdir /app/tensorrt_llm \
             --hostname psqh4m1l0zhx-release \
             --name tensorrt_llm-release-prem \
             --tmpfs /tmp:exec \
             tensorrt_llm/release:latest \
-            python3 examples/llama/build.py --model_dir /mnt/models/llama-2-7b-hf \
+            python3 ./examples/llama/build.py --model_dir /mnt/models/llama-2-7b-hf \
                 --quant_ckpt_path /tensorrt_nvidia_build_04/llama_tp1_rank0.npz \
                 --dtype float16 \
                 --remove_input_padding \
