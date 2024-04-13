e6b60151518d67bea6ef4886d4e076670c7f56e6
Anindyadeep Sannigrahi <anindyadeepsannigrahi@Anindyadeeps-MacBook-Pro.local>
2024-04-13 13:03:00 +0530

    Added another common utility to build chat templates for model

--- common/base.py
+++ common/base.py
@@ -75,7 +75,9 @@ class BaseBenchmarkClass(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def preprocess(self, prompt: str, chat_mode: bool = False):
+    def preprocess(
+        self, prompt: str, chat_mode: bool = True, for_benchmarks: bool = True
+    ):
         """
         Should return a dict
             - prompt: (str) The prompt
@@ -116,11 +118,31 @@ class BaseBenchmarkClass(ABC):
     def on_exit(self):
         pass
 
+    def get_chat_template_with_instruction(
+        self, prompt: str, for_benchmarks: bool = True
+    ):
+        if not for_benchmarks:
+            system_content = "You answers should always be to the point, precise and not more than 2 sentences strictly"
+            if self.model_name == "mistral":
+                template = [
+                    {"role": "user", "content": system_content},
+                    {"role": "assistant", "content": "Sure, noted."},
+                    {"role": "user", "content": prompt},
+                ]
+            else:
+                template = [
+                    {"role": "system", "content": system_content},
+                    {"role": "user", "content": prompt},
+                ]
+            return template
+        else:
+            return [{"role": "user", "content": prompt}]
+
     def _benchmark_cuda(self, prompt: str, max_tokens: int, temperature: float):
         start_event = torch.cuda.Event(enable_timing=True)
         end_event = torch.cuda.Event(enable_timing=True)
 
-        inputs = self.preprocess(prompt=prompt)
+        inputs = self.preprocess(prompt=prompt, for_benchmarks=True)
 
         with self.memory_tracker.track():
             torch.cuda.synchronize()
@@ -162,6 +184,11 @@ class BaseBenchmarkClass(ABC):
         self.on_exit()
 
     def get_answers(self):
+        try:
+            self.model is not None
+        except AttributeError as _:  # noqa
+            self.load_model_and_tokenizer()
+
         self.logger.info("=> Running quality checks for LLM")
 
         for question in tqdm(self.questions, total=len(self.questions)):
@@ -170,7 +197,7 @@ class BaseBenchmarkClass(ABC):
             temperature = question["temperature"]
             expected = question["expected"]
 
-            inputs = self.preprocess(prompt=prompt)
+            inputs = self.preprocess(prompt=prompt, for_benchmarks=False)
             output_dict = self.run_model(
                 inputs=inputs, max_tokens=max_tokens, temperature=temperature
             )
