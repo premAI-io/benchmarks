64c97edb39fbfe5559335f3cfcbbac80f6be19dc
Anindyadeep <proanindyadeep@gmail.com>
2024-04-17 19:18:24 +0530

    removed docker support and added mistral support

--- bench_ctranslate/bench.sh
+++ bench_ctranslate/bench.sh
@@ -6,19 +6,16 @@
 #
 # Usage: ./bench.sh [OPTIONS]
 # OPTIONS:
-#   -p, --prompt      Prompt for benchmarks (default: 'Write an essay about the transformer model architecture')
-#   -r, --repetitions Number of repetitions for benchmarks (default: 10)
-#   -m, --max_tokens  Maximum number of tokens for benchmarks (default: 512)
-#   -d, --device      Device for benchmarks (possible values: 'metal', 'cuda', and 'cpu', default: 'cuda')
-#   -lf, --log_file   Logging file name.
-#   -md, --models_dir Models directory.
-#   -h, --help        Show this help message
+#   -p, --prompt        Prompt for benchmarks (default: 'Write an essay about the transformer model architecture')
+#   -r, --repetitions   Number of repetitions for benchmarks (default: 10)
+#   -m, --max_tokens    Maximum number of tokens for benchmarks (default: 512)
+#   -d, --device        Device for benchmarks (possible values: 'metal', 'cuda', and 'cpu', default: 'cuda')
+#   -n, --model_name    The name of the model to benchmark (possible values: 'llama' for using Llama2, 'mistral' for using Mistral 7B v0.1)
+#   -lf, --log_file     Logging file name.
+#   -h, --help          Show this help message
 ########################################################################################################
 
 set -euo pipefail
-
-CURRENT_DIR="$(pwd)"
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 print_usage() {
     echo "Usage: $0 [OPTIONS]"
@@ -27,11 +24,16 @@ print_usage() {
     echo "  -r, --repetitions   Number of repetitions for benchmarks (default: 10)"
     echo "  -m, --max_tokens    Maximum number of tokens for benchmarks (default: 512)"
     echo "  -d, --device        Device for benchmarks (possible values: 'metal', 'cuda', and 'cpu', default: 'cuda')"
+    echo "  -n, --model_name    The name of the model to benchmark (possible values: 'llama' for using Llama2, 'mistral' for using Mistral 7B v0.1)"
     echo "  -lf, --log_file     Logging file name."
-    echo "  -md, --models_dir   Models directory."
     echo "  -h, --help          Show this help message"
     exit 1
 }
+
+CURRENT_DIR="$(pwd)"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "$SCRIPT_DIR"
 
 check_platform() {
     local platform
@@ -42,6 +44,17 @@ check_platform() {
         echo "Running on Mac OS."
     else
         echo "Unknown platform."
+        exit 1
+    fi
+}
+
+check_cuda() {
+    if command -v nvcc &> /dev/null
+    then
+        echo -e "\nUsing CUDA"
+        nvcc --version
+    else
+        echo -e "\nCUDA is not available."
         exit 1
     fi
 }
@@ -58,8 +71,25 @@ check_python() {
 }
 
 setup() {
-    echo -e "\nSetting up with $SCRIPT_DIR/setup.sh..."
-    bash "$SCRIPT_DIR"/setup.sh "$1"
+    local MODEL_NAME="${1:-llama}"
+
+    if [[ "$MODEL_NAME" == "llama" ]]; then
+        local model_pattern="$CURRENT_DIR/models/llama-2-7b-chat-ctranslate2-*"
+    elif [[ "$MODEL_NAME" == "mistral" ]]; then
+        local model_pattern="$CURRENT_DIR/models/mistral-7b-v0.1-instruct-ctranslate2-*"
+    else
+        echo "No such model is supported"
+        exit 1
+    fi
+
+    matching_dirs=$(ls -d "$model_pattern" 2>/dev/null)
+
+    if [ -n "$matching_dirs" ]; then
+        echo "Already exists skipping setup"
+    else
+        echo -e "\nSetting up with $SCRIPT_DIR/setup.sh..."
+        bash "$SCRIPT_DIR"/setup.sh "$MODEL_NAME"
+    fi
 }
 
 run_benchmarks() {
@@ -67,20 +97,16 @@ run_benchmarks() {
     local REPETITIONS="$2"
     local MAX_TOKENS="$3"
     local DEVICE="$4"
-    local LOG_FILENAME="$5"
-    local MODELS_DIR="$6"
+    local MODEL_NAME="$5"
 
     # shellcheck disable=SC1091
     source "$SCRIPT_DIR/venv/bin/activate"
-
     "$PYTHON_CMD" "$SCRIPT_DIR"/bench.py \
         --prompt "$PROMPT" \
         --repetitions "$REPETITIONS" \
         --max_tokens "$MAX_TOKENS" \
-        --log_file "$LOG_FILENAME" \
-        --models_dir "$MODELS_DIR" \
+        --model_name "$MODEL_NAME" \
         --device "$DEVICE"
-
 }
 
 # Parse command-line arguments
@@ -104,18 +130,20 @@ while [ "$#" -gt 0 ]; do
                 "cuda" | "metal" | "cpu")
                     ;;
                 *)
-                    echo "Invalid value for --device. Please use 'cuda', 'gpu' or 'cpu'."
+                    echo "Invalid value for --device. Please use 'cuda', 'cpu' or 'metal'."
                     print_usage
                     ;;
             esac
+            if [ "$DEVICE" == "cuda" ]; then
+                check_cuda
+            else
+                echo "Not supported for $DEVICE"
+                exit 1
+            fi
             shift 2
             ;;
-        -lf|--log_file)
-            LOG_FILENAME="$2"
-            shift 2
-            ;;
-        -md|--models_dir)
-            MODELS_DIR="$2"
+        -n|--model_name)
+            MODEL_NAME="$2"
             shift 2
             ;;
         -h|--help)
@@ -128,39 +156,16 @@ while [ "$#" -gt 0 ]; do
     esac
 done
 
-# Check if Logs folder exists else Make the logs folder
-LOGS_FOLDER="$CURRENT_DIR/Logs"
-
-if [ -d "$LOGS_FOLDER" ]; then
-    echo "Folder '$LOGS_FOLDER' already exists. Skipping."
-else
-    # Create the folder
-    mkdir "$LOGS_FOLDER"
-    echo "'$LOGS_FOLDER' created."
-fi
 
 # Set default values if not provided
 PROMPT="${PROMPT:-"Write an essay about the transformer model architecture"}"
 REPETITIONS="${REPETITIONS:-10}"
 MAX_TOKENS="${MAX_TOKENS:-512}"
 DEVICE="${DEVICE:-'cuda'}"
-LOG_FILENAME="${LOG_FILENAME:-"/mnt/Logs/benchmark_ctranslate_$(date +'%Y%m%d%H%M%S').log"}"
-MODELS_DIR="${MODELS_DIR:-"/mnt/models"}"
+MODEL_NAME="${MODEL_NAME:-"llama"}"
 
 check_platform
 check_python
-setup "$MODELS_DIR"
+setup "$MODEL_NAME"
 
-docker run -it \
-    --gpus all \
-    -v "$(pwd)/models:/mnt/models" \
-    -v "$SCRIPT_DIR:/mnt/scripts" \
-    -v "$LOGS_FOLDER:/mnt/Logs" \
-    prem-ctranslate2:latest \
-    python3 -u "/mnt/scripts/bench.py" \
-        --prompt "$PROMPT" \
-        --repetitions "$REPETITIONS" \
-        --max_tokens "$MAX_TOKENS" \
-        --log_file "$LOG_FILENAME" \
-        --models_dir "$MODELS_DIR" \
-        --device "$DEVICE"
+run_benchmarks "$PROMPT" "$REPETITIONS" "$MAX_TOKENS" "$DEVICE" "$MODEL_NAME"
