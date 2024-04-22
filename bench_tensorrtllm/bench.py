80e18f8997b1f0d8997a7c79be7972ea916b2127
Anindyadeep <proanindyadeep@gmail.com>
2024-04-22 21:48:29 +0530

    TRT LLM using base class with mistral and memory profiling support

--- bench_tensorrtllm/bench.py
+++ bench_tensorrtllm/bench.py
@@ -1,205 +1,155 @@
-import argparse
-import json
-import logging
+import os
 import sys
-import time
-from collections import defaultdict
-from pathlib import Path
-from typing import Optional
 
-import numpy as np
 import tensorrt_llm
 import torch
-from tensorrt_llm.runtime import ModelConfig, SamplingConfig
+from tensorrt_llm.runtime import ModelRunnerCpp
 from transformers import AutoTokenizer
 
-logging.getLogger("tensorrt_llm").setLevel(logging.ERROR)
-logging.basicConfig(
-    stream=sys.stdout,
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
+sys.path.append("/mnt")
+sys.path.append("/mnt/benchmarks/")
+
+from common.base import BaseBenchmarkClass  # noqa
+from common.utils import launch_cli, make_report  # noqa
 
 
-def log_and_print(message: str) -> None:
-    print(message)
-    logging.info(message)
-
-
-class LlamaTensorRTMBenchmark:
+class TensorRTLLMBenchmark(BaseBenchmarkClass):
     def __init__(
         self,
         model_path: str,
-        tokenizer_path: str,
+        model_name: str,
+        benchmark_name: str,
         precision: str,
-        device: Optional[str] = "cuda",
+        device: str,
+        experiment_name: str,
     ) -> None:
-        assert device == "cuda", ValueError("Device other CUDA is not Supported")
-        assert precision in ["fp32", "fp16", "int8", "int4"], ValueError(
-            "Supported Precision: 'fp32', 'fp16', 'int8' and 'int4'"
+        assert precision in ["float16", "int8", "int4"], ValueError(
+            "Supported precision: 'float16', 'int8' and 'int4'"
         )
-
-        self.engine_dir_path = Path(model_path)
-        engine_files = list(self.engine_dir_path.glob("*.engine"))
-
-        if len(engine_files) == 0:
-            raise ValueError(f"Model path: {model_path} does not consist .engine file")
-
-        self.engine_path = engine_files[0]
-        self.config_path = self.engine_dir_path / "config.json"
-
-        self.precision, self.device, self.tokenizer_path = (
-            precision,
-            device,
-            tokenizer_path,
+        super().__init__(
+            model_name=model_name,
+            model_path=model_path,
+            benchmark_name=benchmark_name,
+            experiment_name=experiment_name,
+            precision=precision,
+            device=device,
+            root_folder="/mnt/benchmarks",
         )
-        self.results = []
+        self.runtime_rank = tensorrt_llm.mpi_rank()
+        if model_name == "llama":
+            self.tokenizer_folder = os.path.join(
+                self.root_folder, "models", "llama-2-7b-chat-hf"
+            )
+        else:
+            self.tokenizer_folder = os.path.join(
+                self.root_folder, "models", "mistral-7b-v0.1-instruct-hf"
+            )
 
-    def load_model(self) -> None:
-        with open(self.config_path) as f:
-            config = json.load(f)
+    def load_model_and_tokenizer(self):
+        self.tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_folder)
+        if self.tokenizer.pad_token_id is None:
+            self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
+        self.pad_id = self.tokenizer.pad_token_id
+        self.end_id = self.tokenizer.eos_token_id
 
-        use_gpt_attention_plugin = config["plugin_config"]["gpt_attention_plugin"]
-        remove_input_padding = config["plugin_config"]["remove_input_padding"]
-        tp_size = config["builder_config"]["tensor_parallel"]
-        pp_size = config["builder_config"]["pipeline_parallel"]
-        world_size = tp_size * pp_size
-
-        num_heads = config["builder_config"]["num_heads"] // tp_size
-        hidden_size = config["builder_config"]["hidden_size"] // tp_size
-        vocab_size = config["builder_config"]["vocab_size"]
-        num_layers = config["builder_config"]["num_layers"]
-        num_kv_heads = config["builder_config"].get("num_kv_heads", num_heads)
-        paged_kv_cache = config["plugin_config"]["paged_kv_cache"]
-
-        num_kv_heads = (num_kv_heads + tp_size - 1) // tp_size
-
-        model_config = ModelConfig(
-            num_heads=num_heads,
-            num_kv_heads=num_kv_heads,
-            hidden_size=hidden_size,
-            vocab_size=vocab_size,
-            num_layers=num_layers,
-            gpt_attention_plugin=use_gpt_attention_plugin,
-            paged_kv_cache=paged_kv_cache,
-            remove_input_padding=remove_input_padding,
+        # load the runner kawargs
+        runner_kwargs = dict(
+            engine_dir=self.model_path,
+            rank=self.runtime_rank,
+            max_batch_size=1,
+            max_input_len=512,
+            max_output_len=512,
+            max_beam_width=1,
+            max_attention_window_size=None,
+            sink_token_length=None,
         )
-
-        world_size = tp_size * pp_size
-        runtime_rank = tensorrt_llm.mpi_rank()
-        runtime_mapping = tensorrt_llm.Mapping(
-            world_size, runtime_rank, tp_size=tp_size, pp_size=pp_size
-        )
-
-        torch.cuda.set_device(runtime_rank % runtime_mapping.gpus_per_node)
-        self.tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_path)
-
-        with open(self.engine_path, "rb") as f:
-            engine_buffer = f.read()
-
-        self.model = tensorrt_llm.runtime.GenerationSession(
-            model_config, engine_buffer, runtime_mapping
-        )
-
+        self.model = ModelRunnerCpp.from_dir(**runner_kwargs)
         return self
 
-    def run_model(self, input_ids, input_lengths, sampling_config):
-        start = time.time()
-        output_ids = self.model.decode(input_ids, input_lengths, sampling_config)
-        delta = time.time() - start
-        return len(output_ids.detach().cpu().numpy()[0][0]) / delta
-
-    def benchmark(self, prompt: str, max_tokens: int, repetitions: int) -> None:
-        input_tokens = []
-        input_tokens.append(self.tokenizer.encode(prompt, add_special_tokens=False))
-
-        input_lengths = torch.tensor(
-            [len(x) for x in input_tokens], dtype=torch.int32, device="cuda"
-        )
-        input_ids = np.concatenate(input_tokens)
-        input_ids = torch.tensor(input_ids, dtype=torch.int32, device="cuda").unsqueeze(
-            0
-        )
-
-        max_input_length = torch.max(input_lengths).item()
-        self.model.setup(input_lengths.size(0), max_input_length, max_tokens, 1)
-
-        sampling_config = SamplingConfig(
-            end_id=2, pad_id=2, num_beams=1, temperature=0.1
-        )
-        for i in range(repetitions):
-            log_and_print(
-                f"Running repetition [{str(i+1).zfill(len(str(repetitions)))}/{repetitions}]"
+    def preprocess(
+        self, prompt: str, chat_mode: bool = True, for_benchmarks: bool = True
+    ):
+        if chat_mode:
+            template = self.get_chat_template_with_instruction(
+                prompt=prompt, for_benchmarks=for_benchmarks
             )
-            tokens_per_second = self.run_model(
-                input_ids, input_lengths, sampling_config
+            prompt = self.tokenizer.apply_chat_template(template, tokenize=False)
+
+        tokenized_input = self.tokenizer.encode(text=prompt)
+        tensor = self.tokenizer.encode(
+            prompt, return_tensors="pt", truncation=True
+        ).squeeze(0)
+        return {
+            "prompt": prompt,
+            "input_tokens": tokenized_input,
+            "tensor": [tensor],
+            "num_input_tokens": len(tokenized_input),
+        }
+
+    def run_model(self, inputs: dict, max_tokens: int, temperature: float) -> dict:
+        tensor = inputs["tensor"]
+        num_input_tokens = inputs["num_input_tokens"]
+
+        with torch.no_grad():
+            output = self.model.generate(
+                tensor,
+                max_new_tokens=max_tokens,
+                temperature=temperature,
+                pad_id=self.pad_id,
+                end_id=self.end_id,
+                return_dict=True,
             )
-            self.results.append(tokens_per_second)
+
+        output_ids = output["output_ids"]
+        output_tokens = output_ids[0][0].detach().cpu().tolist()[num_input_tokens:]
+
+        return {
+            "output_tokens": output_tokens,
+            "num_output_tokens": len(output_tokens),
+        }
+
+    def postprocess(self, output: dict) -> str:
+        output_tokens = output["output_tokens"]
+        output_text = self.tokenizer.decode(output_tokens, skip_special_tokens=True)
+        return output_text
+
+    def on_exit(self):
+        del self.model
         torch.cuda.synchronize()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="TensorRT-LLM Benchmark.")
-    parser.add_argument(
-        "--prompt",
-        type=str,
-        help="The prompt for the model.",
-    )
-    parser.add_argument("--max_tokens", type=int, help="The maximum number of tokens.")
-    parser.add_argument(
-        "--repetitions",
-        type=int,
-        help="The number of repetitions for the benchmark.",
-    )
-    parser.add_argument(
-        "--device",
-        help="Device to use for the benchmark.",
-    )
-    parser.add_argument(
-        "--log_file",
-        type=str,
-        help="Path to the log file for writing logs (in append mode).",
-    )
-    parser.add_argument(
-        "--models_dir",
-        type=str,
-        help="Path to the models directory.",
-    )
+    parser = launch_cli(description="Nvidia TRT-LLM Benchmark.")
     args = parser.parse_args()
-    log_and_print(
-        f"Running benchmark with: max_tokens={args.max_tokens} prompt={args.prompt} "
-        + f"repetitions={args.repetitions} device={args.device}"
+
+    model_folder = "/mnt/benchmarks/models"
+    model_name = (
+        f"{args.model_name}-2-7b-chat-trt"
+        if args.model_name == "llama"
+        else f"{args.model_name}-7b-v0.1-instruct-trt"
     )
-    report = defaultdict(lambda: defaultdict(float))
-    for precision in ["fp32", "fp16", "int8", "int4"]:
-        log_and_print(
-            f"Running TensorRT LLM benchmark (pytorch backend) on Llama with precision: {precision}"
-        )
 
-        llama_tensorrt_benchmark = LlamaTensorRTMBenchmark(
-            model_path=f"{args.models_dir}/llama-2-7b-nvidia_tensorrt_build_{precision}",
-            device=args.device,
-            precision=precision,
-            tokenizer_path=f"{args.models_dir}/llama-2-7b-hf",
-        ).load_model()
+    runner_dict = {
+        "cuda": [
+            {
+                "precision": "float16",
+                "model_path": os.path.join(model_folder, model_name + "-float16"),
+            },
+            {
+                "precision": "int8",
+                "model_path": os.path.join(model_folder, model_name + "-int8"),
+            },
+            {
+                "precision": "int4",
+                "model_path": os.path.join(model_folder, model_name + "-int4"),
+            },
+        ]
+    }
 
-        llama_tensorrt_benchmark.benchmark(
-            max_tokens=args.max_tokens, prompt=args.prompt, repetitions=args.repetitions
-        )
-
-        report["llama_tensorrt_llm"][precision] = {
-            "mean": np.mean(llama_tensorrt_benchmark.results),
-            "std": np.std(llama_tensorrt_benchmark.results),
-        }
-
-    log_and_print("Benchmark Report")
-    with open(args.log_file, "a") as file:
-        for framework, quantizations in report.items():
-            for quantization, stats in quantizations.items():
-                log_and_print(
-                    f"{framework}, {quantization}: {stats['mean']:.2f} ± {stats['std']:.2f}"
-                )
-                print(
-                    f"{framework}, {quantization}: {stats['mean']:.2f} ± {stats['std']:.2f}",
-                    file=file,
-                )
+    make_report(
+        args=args,
+        benchmark_class=TensorRTLLMBenchmark,
+        runner_dict=runner_dict,
+        benchmark_name="Nvidia-TRT-LLM",
+        is_bench_pytorch=False,
+    )
