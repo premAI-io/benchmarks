f6f31ce93a8f3dad5cf86b294e7ab1eb654dd5c3
Anindyadeep <proanindyadeep@gmail.com>
2024-04-22 21:49:05 +0530

    removed old cli args, and some improvements

--- bench_tensorrtllm/bench.sh
+++ bench_tensorrtllm/bench.sh
@@ -2,23 +2,20 @@
 
 ########################################################################################################
 # Script: bench.sh
-# Description: This script runs benchmarks TensorRT-LLM Llama-2 benchmark.
+# Description: This script runs benchmarks Nvidia TensorRT LLM benchmark.
 #
 # Usage: ./bench.sh [OPTIONS]
 # OPTIONS:
-#   -p, --prompt      Prompt for benchmarks (default: 'Write an essay about the transformer model architecture')
-#   -r, --repetitions Number of repetitions for benchmarks (default: 10)
-#   -m, --max_tokens  Maximum number of tokens for benchmarks (default: 512)
-#   -d, --device      Device for benchmarks (possible values: 'metal', 'cuda', and 'cpu', default: 'cuda')
-#   -lf, --log_file   Logging file name.
-#   -md, --models_dir Models directory.
-#   -h, --help        Show this help message
+#   -p, --prompt        Prompt for benchmarks (default: 'Write an essay about the transformer model architecture')
+#   -r, --repetitions   Number of repetitions for benchmarks (default: 10)
+#   -m, --max_tokens    Maximum number of tokens for benchmarks (default: 512)
+#   -d, --device        Device for benchmarks (possible values: 'metal', 'cuda', and 'cpu', default: 'cuda')
+#   -n, --model_name    The name of the model to benchmark (possible values: 'llama' for using Llama2, 'mistral' for using Mistral 7B v0.1)
+#   -lf, --log_file     Logging file name.
+#   -h, --help          Show this help message
 ########################################################################################################
 
 set -euo pipefail
-
-CURRENT_DIR="$(pwd)"
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 print_usage() {
     echo "Usage: $0 [OPTIONS]"
@@ -27,12 +24,14 @@ print_usage() {
     echo "  -r, --repetitions   Number of repetitions for benchmarks (default: 10)"
     echo "  -m, --max_tokens    Maximum number of tokens for benchmarks (default: 512)"
     echo "  -d, --device        Device for benchmarks (possible values: 'metal', 'cuda', and 'cpu', default: 'cuda')"
+    echo "  -n, --model_name    The name of the model to benchmark (possible values: 'llama' for using Llama2, 'mistral' for using Mistral 7B v0.1)"
     echo "  -lf, --log_file     Logging file name."
-    echo "  -md, --models_dir   Models directory."
     echo "  -h, --help          Show this help message"
     exit 1
 }
 
+CURRENT_DIR="$(pwd)"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 check_cuda() {
     if command -v nvcc &> /dev/null
@@ -58,52 +57,13 @@ check_platform() {
     fi
 }
 
-check_python() {
-    if command -v python &> /dev/null; then
-        PYTHON_CMD="python"
-    elif command -v python3 &> /dev/null; then
-        PYTHON_CMD="python3"
-    else
-        echo "Python is not installed."
-        exit 1
-    fi
-}
 
 setup() {
-
-    # Check if Logs folder exists else Make the logs folder
-    LOGS_FOLDER="$CURRENT_DIR/Logs"
-
-    if [ -d "$LOGS_FOLDER" ]; then
-        echo "Folder '$LOGS_FOLDER' already exists. Skipping."
-    else
-        # Create the folder
-        mkdir "$LOGS_FOLDER"
-        echo "'$LOGS_FOLDER' created."
-    fi
-
+    local MODEL_NAME="${1:-llama}"
     echo -e "\nSetting up with $SCRIPT_DIR/setup.sh..."
-    bash "$SCRIPT_DIR"/setup.sh
+    bash "$SCRIPT_DIR/setup.sh" "$MODEL_NAME"
 }
 
-run_benchmarks() {
-    # this will change
-    local PROMPT="$1"
-    local REPETITIONS="$2"
-    local MAX_TOKENS="$3"
-    local DEVICE="$4"
-    local LOG_FILENAME="$5"
-    local MODELS_DIR="$6"
-
-    # shellcheck disable=SC1091
-    "$PYTHON_CMD" "$SCRIPT_DIR"/bench.py \
-        --prompt "$PROMPT" \
-        --repetitions "$REPETITIONS" \
-        --max_tokens "$MAX_TOKENS" \
-        --log_file "$LOG_FILENAME" \
-        --models_dir "$MODELS_DIR" \
-        --device "$DEVICE"
-}
 
 # Parse command-line arguments
 while [ "$#" -gt 0 ]; do
@@ -126,7 +86,7 @@ while [ "$#" -gt 0 ]; do
                 "cuda" | "metal" | "cpu")
                     ;;
                 *)
-                    echo "Invalid value for --device. Please use 'cuda', 'gpu' or 'cpu'."
+                    echo "Invalid value for --device. Please use 'cuda', 'cpu' or 'metal'."
                     print_usage
                     ;;
             esac
@@ -138,12 +98,8 @@ while [ "$#" -gt 0 ]; do
             fi
             shift 2
             ;;
-        -lf|--log_file)
-            LOG_FILENAME="$2"
-            shift 2
-            ;;
-        -md|--models_dir)
-            MODELS_DIR="$2"
+        -n|--model_name)
+            MODEL_NAME="$2"
             shift 2
             ;;
         -h|--help)
@@ -158,15 +114,14 @@ done
 
 check_platform
 check_python
-setup
+setup "$MODEL_NAME"
 
 # Set default values if not provided
 PROMPT="${PROMPT:-"Write an essay about the transformer model architecture"}"
 REPETITIONS="${REPETITIONS:-10}"
 MAX_TOKENS="${MAX_TOKENS:-512}"
 DEVICE="${DEVICE:-'cuda'}"
-LOG_FILENAME="${LOG_FILENAME:-"/mnt/Logs/benchmark_ctranslate_$(date +'%Y%m%d%H%M%S').log"}"
-MODELS_DIR="${MODELS_DIR:-"/mnt/models"}"
+MODEL_NAME="${MODEL_NAME:-"llama"}"
 
 
 docker run \
@@ -175,17 +130,11 @@ docker run \
     --ulimit memlock=-1 \
     --ulimit stack=67108864 \
     -e PYTHONUNBUFFERED=1 \
-    -v "$(pwd)/models:/mnt/models" \
-    -v "$SCRIPT_DIR:/mnt/scripts" \
-    -v "$LOGS_FOLDER:/mnt/Logs" \
-    -v "$SCRIPT_DIR"/TensorRT-LLM:/code/tensorrt_llm \
-    --env "CCACHE_DIR=/code/tensorrt_llm/cpp/.ccache" \
-    --env "CCACHE_BASEDIR=/code/tensorrt_llm" \
-    tensorrt_llm/release:latest \
-    python3 -u "/mnt/scripts/bench.py" \
+    -v "$CURRENT_DIR:/mnt/benchmarks" \
+    -it tensorrt_llm/release:latest \
+    python3 -u "/mnt/benchmarks/bench_tensorrtllm/bench.py" \
         --prompt "$PROMPT" \
         --repetitions "$REPETITIONS" \
         --max_tokens "$MAX_TOKENS" \
-        --log_file "$LOG_FILENAME" \
-        --models_dir "$MODELS_DIR" \
+        --model_name "$MODEL_NAME" \
         --device "$DEVICE"
