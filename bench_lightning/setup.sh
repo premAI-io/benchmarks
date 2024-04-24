a92a3bc61730d495ac66421f8abf3af90cebed39
Anindyadeep <proanindyadeep@gmail.com>
2024-04-24 20:41:33 +0530

    Lightning AI Mistral and memory integration  (#174)
    * Added qualitative comparision of quantity for litgpt
    
    * Using base class with mistral support and memory support
    
    * small cli improvements, removed old arguments
    
    * removed convert logic with latest litgpt
    
    * Added latest inference logic code
    
    * pinned version for dependencies
    
    * Added latest method of installation and model conversions with litgpt
    
    * added performance benchmarks info in litgpt
    
    * updated the memory usage and token per seconds
    
    * chore: minor improvements and added latest info about int4

--- bench_lightning/setup.sh
+++ bench_lightning/setup.sh
@@ -6,6 +6,10 @@
 # requirements.
 ################################################################################
 
+CURRENT_DIR="$(pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+VENV_DIR="$SCRIPT_DIR/venv"
+
 check_python() {
     if command -v python &> /dev/null; then
         PYTHON_CMD="python"
@@ -26,17 +30,9 @@ setup_environment() {
         source "$VENV_DIR/bin/activate"
         pip install --upgrade pip > /dev/null
 
-        # clone the repo
-        if [ -d "$SCRIPT_DIR/lit-gpt" ]; then
-            echo "lit-gpt folder already exists."
-        else
-            git clone https://github.com/Lightning-AI/lit-gpt.git "$SCRIPT_DIR/lit-gpt"
-        fi
-
         # install everything
-        pip install -r "$SCRIPT_DIR/lit-gpt/requirements-all.txt" --no-cache-dir > /dev/null
+        pip install 'litgpt[all] @ git+https://github.com/Lightning-AI/litgpt'
         pip install -r "$SCRIPT_DIR/requirements.txt" --no-cache-dir > /dev/null
-        pip install -e "$SCRIPT_DIR/lit-gpt" --no-cache-dir > /dev/null
         echo "Successfully installed lit-gpt and it's dependencies"
     else
         # shellcheck disable=SC1091
@@ -45,32 +41,43 @@ setup_environment() {
 }
 
 convert_hf_to_litgpt() {
-    local HF_WEIGHTS_FOLDER="$1"
-    local LITGPT_WEIGHTS_FOLDER="$2"
+    local MODEL_NAME="$1"
 
-    if [ -d "$LIT_GPT_MODELS_DIR" ]; then
-        echo "Already converted"
+    # This trick is done because LitGPT expects specific folder name / checkpoint_dir
+    # Llama-2-7b-chat-hf or Mistral-7B-Instruct-v0.1
+    TEMP_DIR=""
+    LITGPT_DIR=""
+    BACK_TO_DIR=""
+
+    if [ "$MODEL_NAME" = "llama" ]; then
+        TEMP_DIR="$CURRENT_DIR/models/Llama-2-7b-chat-hf"
+        LITGPT_DIR="$CURRENT_DIR/models/llama-2-7b-chat-litgpt"
+        BACK_TO_DIR="$CURRENT_DIR/models/llama-2-7b-chat-hf"
+    elif [ "$MODEL_NAME" = "mistral" ]; then
+        TEMP_DIR="$CURRENT_DIR/models/Mistral-7B-Instruct-v0.1"
+        LITGPT_DIR="$CURRENT_DIR/models/mistral-7b-v0.1-instruct-litgpt"
+        BACK_TO_DIR="$CURRENT_DIR/models/mistral-7b-v0.1-instruct-hf"
     else
-        if [ -d "$SCRIPT_DIR/lit-gpt" ]; then
-            mkdir "$LITGPT_WEIGHTS_FOLDER"
-            "$PYTHON_CMD" "$SCRIPT_DIR/convert.py" --checkpoint_dir "$HF_WEIGHTS_FOLDER"
-            mv "$HF_WEIGHTS_FOLDER/lit_config.json" "$HF_WEIGHTS_FOLDER/lit_model.pth" "$LITGPT_WEIGHTS_FOLDER"
-            cp "$HF_WEIGHTS_FOLDER/tokenizer.model" "$LITGPT_WEIGHTS_FOLDER"
-        else
-            echo "Please install the repo first and then go for conversion"
-            exit 1
-        fi
+        echo "Invalid MODEL_NAME. Supported values: 'llama', 'mistral'"
+        exit 1
+    fi
+
+    if [ -d "$LITGPT_DIR" ]; then
+        echo "Already converted"
+        exit 0
+    else
+        mv "$BACK_TO_DIR" "$TEMP_DIR"
+        mkdir -p "$LITGPT_DIR"
+        litgpt convert to_litgpt --checkpoint_dir "$TEMP_DIR"
+        mv "$TEMP_DIR/model_config.yaml" "$TEMP_DIR/lit_model.pth" "$LITGPT_DIR/"
+        cp -r "$TEMP_DIR/tokenizer.model" "$TEMP_DIR/tokenizer_config.json" "$LITGPT_DIR/"
+        mv "$TEMP_DIR" "$BACK_TO_DIR"
     fi
 }
 
 
-CURRENT_DIR="$(pwd)"
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-VENV_DIR="$SCRIPT_DIR/venv"
-HF_MODELS_DIR="${HF_MODELS_DIR:-"models/llama-2-7b-hf"}"
-LIT_GPT_MODELS_DIR="${LIT_GPT_MODELS_DIR:-"models/llama-2-7b-lit-gpt"}"
+MODEL_NAME="$1"
 
 check_python
 setup_environment
-convert_hf_to_litgpt "$CURRENT_DIR/$HF_MODELS_DIR" "$CURRENT_DIR/$LIT_GPT_MODELS_DIR"
+convert_hf_to_litgpt "$MODEL_NAME"
