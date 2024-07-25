cca95bbba86e8547579dd7bf82bac9aa0669e40c
Anindyadeep <proanindyadeep@gmail.com>
2024-07-26 00:07:23 +0530

    refactor setup bash file to install latest llama cpp for cpu and cuda

--- bench_llamacpp/setup.sh
+++ bench_llamacpp/setup.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 ################################################################################
-# Script: setup.sh <DEVICE>
+# Script: setup.sh <DEVICE> <MODEL_NAME>
 # Description: Automates the setup of a virtual environment and installs project
 # requirements.
 ################################################################################
 
 set -euo pipefail
+
+# Set default folder paths for AWQ weights
+CURRENT_DIR="$(pwd)"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+VENV_DIR="$SCRIPT_DIR/venv"
+MODELS_DIR="$CURRENT_DIR/models"
+LLAMA2_GGUF_WEIGHTS_DIR="$MODELS_DIR/llama-2-7b-chat-gguf"
+MISTRAL_GGUF_WEIGHTS_DIR="$MODELS_DIR/mistral-7b-v0.1-instruct-gguf"
 
 check_python() {
     if command -v python &> /dev/null; then
@@ -19,65 +27,57 @@ check_python() {
     fi
 }
 
-clone_and_build_llama() {
-    local DEVICE="$1"
-    local VENV_DIR="$2"
-    local SCRIPT_DIR="$3"
+download_gguf_weights() {
+    local MODEL_NAME="$1"
+    local DOWNLOAD_DIR
 
-    if [ "$#" -ne 3 ]; then
-        echo "Usage: $0 <DEVICE> <ENV> <SCRIPT_DIR>"
-        exit 1
-    fi
-
-    case "$DEVICE" in
-        cuda)
-            export LLAMA_CUBLAS=on
+    case "$MODEL_NAME" in
+        llama)
+            DOWNLOAD_DIR="$LLAMA2_GGUF_WEIGHTS_DIR"
+            MODEL_IDENTIFIER="TheBloke/Llama-2-7B-Chat-GGUF"
+            MODEL_FILE_4BIT="llama-2-7b-chat.Q4_K_M.gguf"
+            MODEL_FILE_8BIT="llama-2-7b-chat.Q8_0.gguf"
             ;;
-        metal)
-            export LLAMA_METAL=on
-            ;;
-        cpu)
-            return 0
+        mistral)
+            DOWNLOAD_DIR="$MISTRAL_GGUF_WEIGHTS_DIR"
+            MODEL_IDENTIFIER="TheBloke/Mistral-7B-Instruct-v0.1-GGUF"
+            MODEL_FILE_4BIT="mistral-7b-instruct-v0.1.Q4_K_M.gguf"
+            MODEL_FILE_8BIT="mistral-7b-instruct-v0.1.Q8_0.gguf"
             ;;
         *)
-            echo "Unsupported DEVICE: $DEVICE"
-            return 1
+            echo "Invalid MODEL_NAME. Supported values: 'llama', 'mistral'"
+            exit 1
             ;;
     esac
 
-    local LIBLLAMA_FILE="$VENV_DIR/libllama_$DEVICE.so"
-
-    if [ -e "$LIBLLAMA_FILE" ]; then
-        echo "File $LIBLLAMA_FILE exists."
-        exit 0
+    if [ ! -d "$DOWNLOAD_DIR" ]; then
+        huggingface-cli download "$MODEL_IDENTIFIER" "$MODEL_FILE_4BIT" --local-dir "$DOWNLOAD_DIR" --local-dir-use-symlinks False
+        huggingface-cli download "$MODEL_IDENTIFIER" "$MODEL_FILE_8BIT" --local-dir "$DOWNLOAD_DIR" --local-dir-use-symlinks False
+    else
+        echo "Weights for $MODEL_NAME already downloaded."
     fi
+}
 
-    # Remove existing llama.cpp directory if it exists
-    if [ -d "$SCRIPT_DIR/llama.cpp" ]; then
-        echo "Removing existing llama.cpp directory..."
-        rm -rf "$SCRIPT_DIR"/llama.cpp
-    fi
+clone_and_build_llama() {
+    local DEVICE="$1"
 
-    git clone --depth=1 https://github.com/ggerganov/llama.cpp "$SCRIPT_DIR"/llama.cpp
-    cd "$SCRIPT_DIR"/llama.cpp
-
-    # Build llama.cpp
-    make clean > /dev/null
     echo "Building llama.cpp..."
-    make libllama.so > /dev/null
-    cp libllama.so "$LIBLLAMA_FILE"
-    cd "$SCRIPT_DIR"
-
-    rm -rf "$SCRIPT_DIR"/llama.cpp
+    if [ "$DEVICE" == "cuda" ]; then
+        pip install llama-cpp-python \
+  --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu121
+    else
+        pip install llama-cpp-python \
+  --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cpu
+    fi
 }
 
 # CLI Args
 DEVICE="$1"
+MODEL_NAME="$2"
 
 # Define directory paths
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 VENV_DIR="$SCRIPT_DIR/venv"
-LIBLLAMA_FILE="$VENV_DIR/libllama_$DEVICE.so"
 
 check_python
 
@@ -87,10 +87,13 @@ if [ ! -d "$VENV_DIR" ]; then
     # shellcheck disable=SC1091
     source "$VENV_DIR/bin/activate"
     pip install --upgrade pip > /dev/null
+    clone_and_build_llama "$DEVICE"
     pip install -r "$SCRIPT_DIR/requirements.txt" --no-cache-dir > /dev/null
+    pip install numpy --upgrade 
 else
     # shellcheck disable=SC1091
     source "$VENV_DIR/bin/activate"
+    
 fi
-
-clone_and_build_llama "$DEVICE" "$VENV_DIR" "$SCRIPT_DIR"
+ 
+download_gguf_weights "$MODEL_NAME"
